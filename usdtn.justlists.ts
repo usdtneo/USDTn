a91d594f5a2307a265504dccaee0420d65e8b903
USDTn <usdtneo@hotmail.com>
2025-06-10 17:27:40 -0300

    usdtn.justlists.ts
    archivo necesario

---
+++ usdtn.justlists.ts
@@ -0,0 +1,21 @@
+{
+  "name": "USDTn Official List",
+  "timestamp": "2024-02-20T00:00:00Z",
+  "version": {
+    "major": 1,
+    "minor": 0,
+    "patch": 0
+  },
+  "keywords": ["USDTn", "stablecoin", "TRC20"],
+  "tokens": [
+    {
+      "chainId": 1, // ID de la cadena de Tron (mainnet)
+      "address": "TNVAVSebb1srqtLnHSBsMshFgtu1tboFLA", // Contrato
+      "symbol": "USDTn",
+      "name": "USDTn Token",
+      "decimals": 18, // Ajusta según tu token
+      "logoURI": "https://usdtneo.github.io/USDTn/logo.png", // URL de tu logo (opcional)
+      "tags": ["stablecoin"]
+    }
+  ]
+}
